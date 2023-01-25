bf592889990e945ca8ea4894b0503ef14cd1c826
Erin <eshaver@email.unc.edu>
2023-01-24 20:38:37 -0500

    changing file path to data

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
-    res.end('./public/index.html');
-});
+    res.end(data);
+})
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 

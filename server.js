cef68268d563d4803b9579e5b9bc3cf7951dd3c6
Erin <eshaver@email.unc.edu>
2023-01-24 21:47:18 -0500

    issue the port variable

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+    res.statusCode=200;
     res.setHeader('Content-Type', 'text/html');
     res.end(data);
 })
@@ -39,6 +39,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
     console.log('Server listening on port ${port}');
-})
+    })
 });
 // That's it! You're all done!
